f7d7e488ff78c40a4ce23afd532514c1a6787bdb
inspoy <inspoy@163.com>
2018-03-14 18:17:00 +0800

    upload the project

---
+++ HeadlessL10nExporter/Form1.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
+using OfficeOpenXml;
+using Mono.Data;
+using Mono.Data.Sqlite;
+
+namespace HeadlessL10nExporter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            label5.Hide();
+        }
+
+        private void OnBrowseClick(object sender, EventArgs e)
+        {
+            var openDialog = new OpenFileDialog()
+            {
+                Filter = "Files (*.xlsx)|*.xlsx"
+            };
+            if (openDialog.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Text = openDialog.FileName;
+            }
+        }
+
+        private void OnExportClick(object sender, EventArgs e)
+        {
+            var xlsxPath = textBox1.Text;
+            if (!Check(!string.IsNullOrWhiteSpace(xlsxPath), "Xlsx path is empty"))
+            {
+                return;
+            }
+            var fileInfo = new FileInfo(xlsxPath);
+            if (!Check(fileInfo.Exists, "File does not exists"))
+            {
+                return;
+            }
+            ExcelPackage pack;
+            string errMsg = "";
+            try
+            {
+                pack = new ExcelPackage(fileInfo);
+            }
+            catch (Exception exc)
+            {
+                pack = null;
+                errMsg = exc.Message;
+            }
+            if (!Check(pack != null, $"Cannot open xlsx document({errMsg})"))
+            {
+                return;
+            }
+            var dbPath = "";
+            try
+            {
+                var metaSheet = pack.Workbook.Worksheets["meta"];
+                var mainSheet = pack.Workbook.Worksheets["main"];
+                if (metaSheet == null || mainSheet == null)
+                {
+                    throw new Exception("Cannot find 'meta' and 'main' sheet");
+                }
+                var showName = metaSheet.Cells[2, 1].Value?.ToString();
+                var author = metaSheet.Cells[2, 2].Value?.ToString();
+                var abbr = metaSheet.Cells[2, 3].Value?.ToString();
+                if (string.IsNullOrWhiteSpace(showName) ||
+                    string.IsNullOrWhiteSpace(author) ||
+                    string.IsNullOrWhiteSpace(abbr))
+                {
+                    throw new Exception("Meta info is not filled correctly");
+                }
+                dbPath = showName.ToLower() + "_" + author.ToLower() + ".db";
+                dbPath = Path.GetDirectoryName(xlsxPath) + "\\" + dbPath;
+                label5.Show();
+                label5.Refresh();
+                var done = DoExport(mainSheet, dbPath, showName, author, abbr);
+                label5.Hide();
+                if (!done)
+                {
+                    // 用户取消
+                    return;
+                }
+            }
+            catch (Exception exc)
+            {
+                Check(false, "Export failed, " + exc.Message);
+                label5.Hide();
+                return;
+            }
+
+            pack.Dispose();
+            MessageBox.Show($"Export done! Saved to '{dbPath}'", "Yeah!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            MessageBox.Show("Please manually copy the .db file to the language pack folder!", "Last Step", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private bool Check(bool condition, string message)
+        {
+            if (!condition)
+            {
+                MessageBox.Show(message + ", please check", "Oops...", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            return condition;
+        }
+
+        private bool DoExport(ExcelWorksheet mainSheet, string dbPath, string showName, string author, string abbr)
+        {
+            if (File.Exists(dbPath))
+            {
+                // 文件已存在
+                var result = MessageBox.Show("Database file is already exist! Overwrite?", "Confirm", MessageBoxButtons.YesNo, MessageBoxIcon.Exclamation);
+                if (result == DialogResult.No)
+                {
+                    // 用户选择放弃
+                    return false;
+                }
+                File.Delete(dbPath);
+            }
+            SqliteHelper helper = new SqliteHelper(dbPath);
+            helper.Query("CREATE TABLE `meta` (`ShowName` TEXT, `Author` TEXT, `Abbr` TEXT, `Date` TEXT)");
+            var exportDate = DateTime.UtcNow.ToString("yyyyMMdd");
+            helper.Query($"INSERT INTO `meta` VALUES ('{showName}', '{author}', '{abbr}', '{exportDate}')");
+            helper.Query("CREATE TABLE `main` (`Id` INT, `Alias` TEXT, `Text` TEXT)");
+            int rowNo = 1;
+            StringBuilder sqlString = new StringBuilder();
+            while (true)
+            {
+                rowNo += 1;
+                string id = mainSheet.Cells[rowNo, 1].Value?.ToString();
+                string alias = mainSheet.Cells[rowNo, 2].Value?.ToString();
+                string text = mainSheet.Cells[rowNo, 3].Value?.ToString();
+                if (string.IsNullOrWhiteSpace(id))
+                {
+                    // 没有了
+                    break;
+                }
+                if (string.IsNullOrWhiteSpace(alias))
+                {
+                    // alias为空
+                    continue;
+                }
+                if (string.IsNullOrWhiteSpace(text))
+                {
+                    text = "";
+                }
+                Console.WriteLine("rowNo: " + rowNo);
+                text = text.Replace("'", "''"); // 引号转义
+                sqlString.AppendFormat("INSERT INTO `main` VALUES ({0}, '{1}', '{2}');", id, alias, text);
+            }
+            helper.Query(sqlString.ToString());
+            helper.Dispose();
+            return true;
+        }
+    }
+}
---
+++ HeadlessL10nExporter/SqlHelper.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Mono.Data;
+using Mono.Data.Sqlite;
+
+namespace HeadlessL10nExporter
+{
+    public sealed class SqliteHelper : IDisposable
+    {
+        /// <summary>
+        /// 数据库连接
+        /// </summary>
+        private SqliteConnection m_dbConnection;
+
+        /// <summary>
+        /// 数据库文件名
+        /// </summary>
+        private readonly string m_dbPath;
+
+        /// <summary>
+        /// 构造函数
+        /// </summary>
+        /// <param name="connect">数据库文件名</param>
+        /// <param name="useMemory">事先把数据库加载到内存</param>
+        public SqliteHelper(string connect, bool useMemory = false)
+        {
+            m_dbPath = connect;
+            try
+            {
+                if (useMemory)
+                {
+                    m_dbConnection = new SqliteConnection("URI=file::memory:");
+                    m_dbConnection.Open();
+                    var command = m_dbConnection.CreateCommand();
+                    command.CommandText = string.Format("ATTACH '{0}' AS filedb", connect);
+                    command.ExecuteNonQuery();
+                    command.Dispose();
+                    Console.WriteLine("数据库[内存]已连接:" + m_dbPath);
+                }
+                else
+                {
+                    m_dbConnection = new SqliteConnection("data source=" + connect);
+                    m_dbConnection.Open();
+                    Console.WriteLine("数据库已连接:" + m_dbPath);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("数据库连接失败" + e);
+            }
+        }
+
+        /// <summary>
+        /// 执行SQL查询
+        /// </summary>
+        /// <param name="sql">SQL语句</param>
+        /// <returns>查询结果</returns>
+        public SqliteDataReader Query(string sql)
+        {
+            SqliteCommand dbCommand = m_dbConnection.CreateCommand();
+            dbCommand.CommandText = sql;
+            var ret = dbCommand.ExecuteReader();
+            dbCommand.Dispose();
+            return ret;
+        }
+
+        #region IDisposable Support
+        private bool disposedValue = false; // 要检测冗余调用
+
+        private void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing && m_dbConnection != null)
+                {
+                    m_dbConnection.Close();
+                    m_dbConnection.Dispose();
+                    Console.WriteLine("数据库连接已关闭:" + m_dbPath);
+                }
+                m_dbConnection = null;
+
+                disposedValue = true;
+            }
+        }
+
+        ~SqliteHelper()
+        {
+            // 请勿更改此代码。将清理代码放入以上 Dispose(bool disposing) 中。
+            Dispose(false);
+        }
+
+        // 添加此代码以正确实现可处置模式。
+        public void Dispose()
+        {
+            // 请勿更改此代码。将清理代码放入以上 Dispose(bool disposing) 中。
+            Dispose(true);
+            // 通知GC不再调用终结器
+            GC.SuppressFinalize(this);
+        }
+        #endregion
+    }
+}
